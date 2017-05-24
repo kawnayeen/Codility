45d8aca941ea1ebced773017761a816424f1586c
kawnayeen <kawnayeen.08@gmail.com>
2017-05-24 15:14:53 +0600

    max counter performace problem solved

--- src/main/java/com/kawnayeen/codility/_4_counting_elements/_4_max_counters/MaxCounter.java
+++ src/main/java/com/kawnayeen/codility/_4_counting_elements/_4_max_counters/MaxCounter.java
@@ -1,7 +1,5 @@
 package com.kawnayeen.codility._4_counting_elements._4_max_counters;
 
-import java.util.Arrays;
-
 /**
  * kawnayeen
  * 5/24/17
@@ -10,15 +8,25 @@ public class MaxCounter {
     public int[] solution(int N, int[] A) {
         int[] counters = new int[N];
         int maxValue = 0;
+        int secondMax = 0;
         final int LENGTH = A.length;
 
         for (int i = 0; i < LENGTH; i++) {
             if (A[i] >= 1 && A[i] <= N) {
+                if (counters[A[i] - 1] < secondMax) {
+                    counters[A[i] - 1] = secondMax;
+                }
                 counters[A[i] - 1]++;
                 if (counters[A[i] - 1] > maxValue)
                     maxValue = counters[A[i] - 1];
             } else if (A[i] == (N + 1)) {
-                Arrays.fill(counters, maxValue);
+                secondMax = maxValue;
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (counters[i] < secondMax) {
+                counters[i] = secondMax;
             }
         }
         return counters;
